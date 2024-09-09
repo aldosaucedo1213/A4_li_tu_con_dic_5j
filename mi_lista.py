c88f955a5827ae36bae5d1eec54c4c75d653aaad
aldosaucedo1213 <a.22308051281310@cbtis128.edu.mx>
2024-09-09 10:13:13 -0600

    Add files via upload

---
+++ mi_lista.py
@@ -0,0 +1,18 @@
+
+print("ejemplo de listas")
+arcoiris=["verde","azul","rojo"]
+print(arcoiris)
+longuitud=len(arcoiris)
+print("elementos que contiene la lista arcoiris",longuitud)
+print(f"elementos que contiene la lista arcoiris v2,{longuitud}")
+print("accediendo a un elemento de la lista" )
+print(arcoiris[1])
+print(f"elemento en la posicion 0 es {arcoiris[0]}")
+print("agregar a un elemento de la lista ")
+print(arcoiris)
+arcoiris.append("naranja")
+print(arcoiris)
+print("listar los elementos de la lista ciclo for")
+for saucedo in arcoiris:
+    print(saucedo)
+    
---
+++ mi_tuplas.py
@@ -0,0 +1,8 @@
+print("ejemplos de tupas ")
+canciones=("te amo","el noa noa","amor eterno")
+print(canciones)
+y = list(canciones)
+print(y)
+y[1] = "la puerta negra "
+x = tuple(y)
+print(x)
