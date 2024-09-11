3b8816b2f6b43c9968ae67cddcf4ef23ba98aae5
aldosaucedo1213 <a.22308051281310@cbtis128.edu.mx>
2024-09-11 07:37:41 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,6 @@
+print("ejemplo conjuntos set")
+tipodeportes={"futbol","tenis","basquetbol"}
+print(tipodeportes)
+print("lista elementos del conjunto tipodeportes loop for")
+for saucedo in tipodeportes:
+    print(saucedo)
